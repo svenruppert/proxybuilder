cfe7a27fa485084f1602785aaed0564f6ff712cc
Sven Ruppert <sven.ruppert@gmail.com>
2015-12-15 18:43:04 +0100

    JMS-11 Add Metrics - Proxies to the REST / Servlets

--- modules/static/generated/processors/src/main/java/org/rapidpm/proxybuilder/staticgenerated/processors/StaticVirtualProxyAnnotationProcessor.java
+++ modules/static/generated/processors/src/main/java/org/rapidpm/proxybuilder/staticgenerated/processors/StaticVirtualProxyAnnotationProcessor.java
@@ -2,8 +2,6 @@ package org.rapidpm.proxybuilder.staticgenerated.processors;
 
 import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.FieldSpec;
-import com.squareup.javapoet.TypeName;
-import com.squareup.javapoet.TypeSpec;
 import org.rapidpm.proxybuilder.staticgenerated.annotations.IsGeneratedProxy;
 import org.rapidpm.proxybuilder.staticgenerated.annotations.IsVirtualProxy;
 import org.rapidpm.proxybuilder.staticgenerated.annotations.StaticVirtualProxy;
@@ -17,24 +15,17 @@ import javax.lang.model.element.TypeElement;
  */
 public class StaticVirtualProxyAnnotationProcessor extends BasicStaticProxyAnnotationProcessor<StaticVirtualProxy> {
   @Override
-  protected CodeBlock defineMethodImplementation(final ExecutableElement methodElement, final String methodName2Delegate) {
-    return null;
+  protected void addClassLevelSpecs(final TypeElement typeElement, final RoundEnvironment roundEnv) {
+    typeSpecBuilderForTargetClass.addAnnotation(IsGeneratedProxy.class);
+    typeSpecBuilderForTargetClass.addAnnotation(IsVirtualProxy.class);
+
+    final FieldSpec delegatorFieldSpec = defineDelegatorField(typeElement);
+    typeSpecBuilderForTargetClass.addField(delegatorFieldSpec);
   }
 
   @Override
-  protected void addClassLevelSpecs(final TypeElement typeElement, final RoundEnvironment roundEnv) {
-
-    final TypeName targetTypeName = TypeName.get(typeElement.asType());
-    final TypeSpec.Builder specBuilderForTargetClass = createTypeSpecBuilderForTargetClass(typeElement, targetTypeName);
-
-    specBuilderForTargetClass.addAnnotation(IsGeneratedProxy.class);
-    specBuilderForTargetClass.addAnnotation(IsVirtualProxy.class);
-
-
-    final FieldSpec delegatorFieldSpec = defineDelegatorField(typeElement);
-    specBuilderForTargetClass.addField(delegatorFieldSpec);
-
-
+  protected CodeBlock defineMethodImplementation(final ExecutableElement methodElement, final String methodName2Delegate) {
+    return null;
   }
 
   @Override
