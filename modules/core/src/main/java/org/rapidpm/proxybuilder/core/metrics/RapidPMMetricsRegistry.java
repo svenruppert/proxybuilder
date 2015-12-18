974467393bbca346eb47f20d26c4794554e47de9
Sven Ruppert <sven.ruppert@gmail.com>
2015-12-18 10:50:35 +0100

    JMS-11 Add Metrics - Proxies to the REST / Servlets

--- modules/core/src/main/java/org/rapidpm/proxybuilder/core/metrics/RapidPMMetricsRegistry.java
+++ modules/core/src/main/java/org/rapidpm/proxybuilder/core/metrics/RapidPMMetricsRegistry.java
@@ -1,13 +1,25 @@
 package org.rapidpm.proxybuilder.core.metrics;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by sven on 28.04.15.
  */
 public class RapidPMMetricsRegistry {
+
+
+  public static final TimeUnit DURATION_UNIT = TimeUnit.MILLISECONDS;
+  public static final TimeUnit RATE_UNIT = TimeUnit.MILLISECONDS;
+  private static final Object LOCK = new Object();
   private static RapidPMMetricsRegistry ourInstance = new RapidPMMetricsRegistry();
   private final MetricRegistry metrics = new MetricRegistry();
+
+  private JmxReporter jmxReporter;
+  private ConsoleReporter consoleReporter;
 
   private RapidPMMetricsRegistry() {
   }
@@ -19,4 +31,51 @@ public class RapidPMMetricsRegistry {
   public MetricRegistry getMetrics() {
     return metrics;
   }
+
+  public void startJmxReporter() {
+
+    synchronized (LOCK) {
+      if (jmxReporter == null) {
+        jmxReporter = JmxReporter.forRegistry(metrics)
+            .convertDurationsTo(DURATION_UNIT)
+            .convertRatesTo(RATE_UNIT)
+            .build();
+      }
+      jmxReporter.start();
+    }
+
+  }
+
+  public void stopJmxReporter() {
+    synchronized (LOCK) {
+      if (jmxReporter != null) {
+        jmxReporter.stop();
+        jmxReporter.close();
+        jmxReporter = null;
+      }
+    }
+  }
+
+  public void startConsoleReporter() {
+    synchronized (LOCK) {
+      if (consoleReporter == null) {
+        consoleReporter = ConsoleReporter.forRegistry(metrics)
+            .convertDurationsTo(DURATION_UNIT)
+            .convertRatesTo(RATE_UNIT)
+            .build();
+        consoleReporter.start(5, TimeUnit.SECONDS);
+      }
+    }
+  }
+
+  public void stopConsoleReporter() {
+    synchronized (LOCK) {
+      if (consoleReporter != null) {
+        consoleReporter.stop();
+        consoleReporter.close();
+        consoleReporter = null;
+      }
+    }
+  }
+
 }
