b18666a4458d2c9ed45d447935174efbc33546ea
Sven Ruppert <sven.ruppert@gmail.com>
2016-01-26 14:06:23 +0100

    DDI-12 Scopes

---
+++ modules/static/generated/testusage/src/main/java/junit/org/rapidpm/proxybuilder/staticgenerated/processors/pkg01/ServiceImpl.java
@@ -0,0 +1,32 @@
+package junit.org.rapidpm.proxybuilder.staticgenerated.processors.pkg01;
+
+import org.rapidpm.proxybuilder.staticgenerated.annotations.StaticMetricsProxy;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Sven Ruppert on 09.12.15.
+ */
+@StaticMetricsProxy
+public class ServiceImpl {
+
+  public String doWork(String txt) {
+    return txt + LocalDateTime.now();
+  }
+
+
+  public void doNothing() {
+  }
+
+  public List<String> createList() throws NegativeArraySizeException {
+    return Collections.emptyList();
+  }
+
+  public List<String> createListA() throws Exception {
+    return Collections.emptyList();
+  }
+
+
+}
