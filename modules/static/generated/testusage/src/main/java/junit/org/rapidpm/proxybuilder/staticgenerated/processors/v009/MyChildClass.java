2277e248e961ee016d27b4775816e3dee1120b32
marcus.lang <m.lang@reply.de>
2016-05-09 14:28:45 +0200

    testcase with default constructor in superclass

---
+++ modules/static/generated/testusage/src/main/java/junit/org/rapidpm/proxybuilder/staticgenerated/processors/v009/MyChildClass.java
@@ -0,0 +1,11 @@
+package junit.org.rapidpm.proxybuilder.staticgenerated.processors.v009;
+
+import org.rapidpm.proxybuilder.staticgenerated.annotations.StaticMetricsProxy;
+
+@StaticMetricsProxy
+public class MyChildClass extends MyClass {
+
+  public MyChildClass(String name) {
+    super(name);
+  }
+}
---
+++ modules/static/generated/testusage/src/main/java/junit/org/rapidpm/proxybuilder/staticgenerated/processors/v009/MyClass.java
@@ -0,0 +1,12 @@
+package junit.org.rapidpm.proxybuilder.staticgenerated.processors.v009;
+
+public class MyClass {
+
+  private String name = "";
+
+  public MyClass(String name) {
+    // Default constructor
+    this.name = name;
+  }
+
+}
